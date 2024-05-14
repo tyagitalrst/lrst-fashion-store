c7603bc66da2991cce6f57900d752b3ef7cafe49
tyagita <tyagita.larasati@advotics.com>
2024-05-14 16:29:45 +0700

    update: add cart dropdown feature

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,20 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 
-import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
-import './navigation.styles.scss'
+import "./navigation.styles.scss";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navagiation = () => {
-  const { currentUser } = useContext(UserContext)
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
   /**
    * Outlet: parent route elements to render their child route elements
    */
@@ -17,29 +22,28 @@ const Navagiation = () => {
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <CrwnLogo className="logo"/>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          {
-            currentUser ? (
-              <span className="nav-link" onClick={ signOutUser }>
-                SIGN OUT
-              </span>
-            ) : (
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
             <Link className="nav-link" to="/auth">
-              SIGN IN 
+              SIGN IN
             </Link>
-            )
-          }
-       
+          )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
-  )
-}
+  );
+};
 
-export default Navagiation
+export default Navagiation;
