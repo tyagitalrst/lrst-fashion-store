8e5962e67000bf5b6ef3d42fb92df66896622e99
tyagita <tyagita.larasati@advotics.com>
2024-05-20 14:43:57 +0700

    refactor:redux-thunk

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -2,19 +2,28 @@ import { Fragment } from "react";
 import { useSelector } from "react-redux";
 
 import CategoryPreview from "../../components/category-preview/category-preview.component";
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import Spinner from "../../components/spinner/spinner.component";
+import {
+  selectCategoriesIsLoading,
+  selectCategoriesMap,
+} from "../../store/categories/category.selector";
 
 const CategoriesPreview = () => {
   const categories = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCategoriesIsLoading);
 
   return (
     <Fragment>
-      {Object.keys(categories).map((title) => {
-        const products = categories[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categories).map((title) => {
+          const products = categories[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          );
+        })
+      )}
     </Fragment>
   );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,46 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
 import { rootReducer } from "./root-reducer";
-
-// const loggerMiddleware = (store) => (next) => (action) => {
-//   if (!action.type) return next(action);
-
-//   console.log("type", action.type);
-//   console.log("payload", action.payload);
-//   console.log("currentState", store.getState());
-
-//   next(action);
-
-//   console.log("next state", store.getState());
-// };
+import { rootSaga } from "./root-saga";
 
 /**
  * Enhancer store:
  * action => dispatch
  */
-const middleWares = [logger];
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  sagaMiddleware,
+].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
